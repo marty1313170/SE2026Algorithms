f971563ac71e55f65fe5f1f3f77a0f3600069b85
Martin Han <martin.han1@education.nsw.gov.au>
2026-02-16 10:56:34 +1100

    added new file for function practising

--- d2.py
+++ d2.py
@@ -1,9 +1,15 @@
-# SE2026Algorithms
-Unit 1
+total_cost = 0
+name = " "
 
-YO WSG
+def main():
 
-def menu(themenu):
+    global name
+    name = input("What is your name?")
+    return
+
+def menu():
+
+    global total_cost
 
     b = input("What would you like? Options are \n 1) $4 Pizza 2) $4 Burger 3) $5 Sushi  4) $3 Chips 5) $7 Nachos \n Type the name of the foods you want and seperate them by spaces").lower().split()
 
@@ -19,13 +25,19 @@ def menu(themenu):
         total_cost +=3
     return  
 
-def discount(dis):
+def discount():
+    global total_cost
+    global name
     discount = total_cost - total_cost / 10
 
     if total_cost >= 10:
-        print(discount, a)
+        print(discount, name)
     else:
-        print("$", total_cost, "{nameasker}")
+        print("$", total_cost, name)
 
     return 
+
+main()
+menu()
+discount()
 
